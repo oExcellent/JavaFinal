8f140659727561324a4b264ceecad20d7cd26230
oExcellent <waltm3@mail.broward.edu>
2017-05-04 18:44:56 -0400

    Added first qestion

---
+++ src/myWindow.java
@@ -0,0 +1,38 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class myWindow extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					myWindow frame = new myWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public myWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(250, 250, 500, 500);
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+	}
+
+}
